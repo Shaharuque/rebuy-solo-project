29e0239010e2cf16d470fdf76866d203a5020a1a
unknown <shahrukhamin27@gmail.com>
2023-08-27 01:42:46 +0600

    updated

--- Server/src/controller/chat.ts
+++ Server/src/controller/chat.ts
@@ -24,7 +24,7 @@ export const accessChat: RequestHandler = async (req, res) => {
       .populate("users", "-password") //want to populate the Chat model users property with all of the users info except password
       .populate("latestMessage"); //want to populate Chat model latestMessage property with message all infos cuz initially on id silo property tey
 
-    //want to get the user info of latest message
+    //want to get the user info as well who sent the latest message. as in the Chat model latestMessage property has reference to Message model and Message model sender property has reference to User model so we can populate the sender property with all of the user info except password
     isChat = await User.populate(isChat, {
       path: "latestMessage.sender",
       select: "name avatar email",
@@ -38,20 +38,167 @@ export const accessChat: RequestHandler = async (req, res) => {
       var chatData = {
         chatName: "sender",
         isGroupChat: false,
-        users: [req.user.id, userId],   //logged in user=>req.user.id and jar sathey chat kortey chay=>userId
+        users: [req.user.id, userId], //logged in user=>req.user.id and jar sathey chat kortey chay=>userId
       };
 
-      try{  
-        const creatChat=await Chat.create(chatData)
+      try {
+        const creatChat = await Chat.create(chatData);
 
-        const finalResult=await Chat.find({_id:creatChat._id}).populate('users','-password')
+        const finalResult = await Chat.find({ _id: creatChat._id }).populate(
+          "users",
+          "-password"
+        );
 
-        res.status(200).json(finalResult)
-      }catch(err){
-        res.status(500).json(err)
+        res.status(200).json(finalResult);
+      } catch (err) {
+        res.status(500).json(err);
       }
     }
   } catch (err) {
     res.status(500).json(err);
+  }
+};
+
+//@description     Fetch all chats for a user
+//@route           GET /api/chat/
+//@access          Protected
+export const fetchChats: RequestHandler = async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user.id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results: object) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
+
+
+//@description     Create New Group Chat
+//@route           POST /api/chat/group
+//@access          Protected
+export const createGroupChat: RequestHandler = async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please Fill all the feilds" });
+  }
+
+  var users = JSON.parse(req.body.users);  //user ar id jader sathey group chat create hobey tader id gula array tey ashbey
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send("More than 2 users are required to form a group chat");
+  }
+
+  //logged in user is also a member of the group req.user.id
+  users.push(req.user.id);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
+
+// @desc    Rename Group
+// @route   PUT /api/chat/rename
+// @access  Protected
+export const renameGroup : RequestHandler = async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+};
+
+// @desc    Remove user from Group
+// @route   PUT /api/chat/groupremove
+// @access  Protected
+export const removeFromGroup : RequestHandler = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(removed);
+  }
+};
+
+// @desc    Add user to Group / Leave
+// @route   PUT /api/chat/groupadd
+// @access  Protected
+export const addToGroup : RequestHandler = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(added);
   }
 };
--- Server/src/route/chatRoute.ts
+++ Server/src/route/chatRoute.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken";
-import { accessChat } from "../controller/chat";
+import { accessChat,fetchChats,createGroupChat } from "../controller/chat";
 
 const router=express.Router()
 
 router.post('/',verifyToken,accessChat)
-// router.get('/',verifyToken,fetchChat)
-// router.post('/create/group',verifyToken,createGroupChat)
+router.get('/',verifyToken,fetchChats)
+router.post('/create/group',verifyToken,createGroupChat)
 // router.put('/rename/group',verifyToken,renameGroup)
 // router.put('/remove/from/group',verifyToken,removeFromGroup)
 // router.put('/add/to/group',verifyToken,addToGroup)
